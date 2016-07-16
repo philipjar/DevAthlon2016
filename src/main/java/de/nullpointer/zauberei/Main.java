b11565feb8a3ceb54c01cf6db7d28d744708ebc6
JavaTheHuttt <tim.rademacher@kabelmail.de>
2016-07-16 18:14:57 +0200

    Registered BallEvents

--- src/main/java/de/nullpointer/zauberei/Main.java
+++ src/main/java/de/nullpointer/zauberei/Main.java
@@ -2,6 +2,13 @@ package main.java.de.nullpointer.zauberei;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import main.java.de.nullpointer.zauberei.events.BallEvents;
+
 public class Main extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		new BallEvents(this);
+	}
 
 }
--- src/main/java/de/nullpointer/zauberei/events/BallEvents.java
+++ src/main/java/de/nullpointer/zauberei/events/BallEvents.java
@@ -6,10 +6,16 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+
+import main.java.de.nullpointer.zauberei.Main;
 import main.java.de.nullpointer.zauberei.ball.ThrownQuaffle;
 
 public class BallEvents implements Listener {
-
+	
+	public BallEvents(Main plugin) {
+		plugin.getServer().getPluginManager().registerEvents(this, plugin);
+	}
+	
 	@SuppressWarnings("deprecation")
 	@EventHandler
 	public void onPlayerThrow(PlayerInteractEvent e) {
