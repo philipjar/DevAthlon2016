d3a876fb961f3969bc1bcf1b2ec3bdbe90c2df61
JavaTheHuttt <tim.rademacher@kabelmail.de>
2016-07-16 18:50:58 +0200

    Added start/stop arguments

--- src/main/java/de/nullpointer/zauberei/CommandListener.java
+++ src/main/java/de/nullpointer/zauberei/CommandListener.java
@@ -5,16 +5,32 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public class CommandListener implements CommandExecutor {
-	
+
 	public Main plugin;
-	
+
 	public CommandListener(Main plugin) {
 		this.plugin = plugin;
 	}
-	
+
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		
+		if (cmd.getName().equalsIgnoreCase("quiddich")) {
+			if (args.length >= 1) {
+				
+				if(args[0].equalsIgnoreCase("start")) {
+					// Start game
+					sender.sendMessage("Spiel wurde gestartet! ");
+				}
+				
+				if(args[0].equalsIgnoreCase("stop")) {
+					// Stop game
+					sender.sendMessage("Spiel gestoppt");
+				}
+				
+			} else {
+				// Help
+			}
+		}
 		return false;
 	}
 
