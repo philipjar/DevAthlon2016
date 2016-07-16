cfaba6e5654cc0a21c3c19659c76b6f78c2613fd
philipjar <philipjar@gmail.com>
2016-07-16 18:49:10 +0200

    Changed extending to wrapping

--- src/main/java/de/nullpointer/zauberei/team/QuidditchPlayer.java
+++ src/main/java/de/nullpointer/zauberei/team/QuidditchPlayer.java
@@ -9,7 +9,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
-public class QuidditchPlayer extends CraftPlayer {
+public class QuidditchPlayer {
 	
 	public enum Playertype {
 		BEATER, CHASER, SEEKER, KEEPER
@@ -20,17 +20,18 @@ public class QuidditchPlayer extends CraftPlayer {
 			Material.STICK
 	};
 	
+	private Player player;
 	private Playertype type;
 	private QuidditchTeam team;
 	
 	public QuidditchPlayer(Player player, Playertype type, QuidditchTeam team) {
-		super((CraftServer) Bukkit.getServer(), ((CraftPlayer) player).getHandle());
+		this.player = player;
 		this.type = type;
 		this.team = team;
 	}
 	
 	public void fillInventory() {
-		Inventory inv = getInventory();
+		Inventory inv = player.getInventory();
 		inv.clear();
 		for (Material m : items) {
 			inv.addItem(new ItemStack(m));
