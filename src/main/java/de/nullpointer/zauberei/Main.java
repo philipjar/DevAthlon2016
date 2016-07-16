f7095b69b005dbb26251ed3ce09873044127acf1
JavaTheHuttt <tim.rademacher@kabelmail.de>
2016-07-16 19:12:31 +0200

    Added event registration

--- src/main/java/de/nullpointer/zauberei/Main.java
+++ src/main/java/de/nullpointer/zauberei/Main.java
@@ -3,18 +3,24 @@ package main.java.de.nullpointer.zauberei;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import main.java.de.nullpointer.zauberei.events.BallEvents;
+import main.java.de.nullpointer.zauberei.events.FlyEvents;
+import main.java.de.nullpointer.zauberei.events.InteractEvents;
 
 public class Main extends JavaPlugin {
-	
+
 	CommandListener cListener;
-	
+	QuidditchGame game;
+
 	@Override
 	public void onEnable() {
-		new BallEvents(this);
 		
+		new BallEvents(this);
+		new FlyEvents(this);
+		new InteractEvents(this);
+
 		cListener = new CommandListener(this);
 		getCommand("quiddich").setExecutor(cListener);
-		
+
 	}
 
 }
