da9307cade54ca532b1986540b2f390ef440b8e0
javaTheHuttt <tim.rademacher@kabelmail.de>
2016-07-17 10:45:22 +0200

    Stuff

--- src/main/java/de/nullpointer/zauberei/ball/ThrownQuaffleThread.java
+++ src/main/java/de/nullpointer/zauberei/ball/ThrownQuaffleThread.java
@@ -1,5 +1,9 @@
 package main.java.de.nullpointer.zauberei.ball;
 
+import java.util.ArrayList;
+
+import org.bukkit.Location;
+
 import main.java.de.nullpointer.zauberei.ConfigManager;
 import main.java.de.nullpointer.zauberei.Main;
 import main.java.de.nullpointer.zauberei.Util;
@@ -16,21 +20,34 @@ public class ThrownQuaffleThread implements Runnable {
 	@Override
 	public void run() {
 		
+		Location a0 = ConfigManager.getGate('a', 1);
+		Location a1 = ConfigManager.getGate('a', 2);
+		Location a2 = ConfigManager.getGate('a', 3);
+		Location b0 = ConfigManager.getGate('b', 1);
+		Location b1 = ConfigManager.getGate('b', 2);
+		Location b2 = ConfigManager.getGate('b', 3);
+		
+		ArrayList<Location> goalLocs = new ArrayList<>();
+		goalLocs.add(a0);
+		goalLocs.add(a1);
+		goalLocs.add(a2);
+		goalLocs.add(b0);
+		goalLocs.add(b1);
+		goalLocs.add(b2);
+		
 		while(!quaffle.isOnGround()) {
-			
 			
 			for (QuidditchPlayer qp: Main.game.getAllPlayers()) {
 				if (Util.isNearTo(qp.getPlayer().getEyeLocation(), quaffle.getLocation(), 2)) {
 					quaffle.remove();
 					qp.giveQuaffle();
+					break;
 				}
 			}
 			
-			/*
-			 * 
-			 * Check for goal hit --> Call goal
-			 * 
-			 */
+			for (Location l : goalLocs) {
+				if (Util.isNearTo(l, quaffle.getLocation(), 2));
+			}
 			
 			try { Thread.sleep(1); } catch (InterruptedException e) { e.getMessage(); } 
 			
